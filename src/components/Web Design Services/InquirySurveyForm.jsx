90ca30f00cfbeb413b0847cf4f5dc8dd7d9a5005
Braden <fish0859@gmail.com>
2020-06-20 13:38:11 -0600

    late commit

--- src/components/Web Design Services/InquirySurveyForm.jsx
+++ src/components/Web Design Services/InquirySurveyForm.jsx
@@ -224,7 +224,7 @@ export default function Contact() {
                 type="checkbox"
                 checked={data.adCopy}
                 onChange={onChangeHandler}
-              />
+              />˝
               {"  "} Ad-Copy Writing
             </label>
             <label for="webMaintenance">
