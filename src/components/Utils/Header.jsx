2cee35b6e1263a4a0db5861a2bb437ebc1d6a528
Braden <fish0859@gmail.com>
2020-06-22 17:44:35 -0600

    changed header and footer styling

--- src/components/Utils/Header.jsx
+++ src/components/Utils/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import blueSmoke from "../../images/blue-smoke.png";
-// import blueSmoke from "../../images/background_image_w_flame-BSlogo-only.png";
+// import blueSmoke from "../../images/blue-background.png";
+import flame from "../../images/flame.png";
 
 export default function Header() {
   return (
     <header>
-      <img src={blueSmoke} alt="blue-smoke" />
-      <p>Digital and Printed Media</p>
-      <div className="sub-text">
-        <span>What do your customers see...</span>
-        <span>...Are you SURE?</span>
+      {/* <img src={blueSmoke} alt="blue-smoke" /> */}
+      <img src={flame} alt="logo-flame" id="flame" />
+      <div>
+        <img src={blueSmoke} alt="blue-smoke" />
+        <p>Digital and Printed Media</p>
+        <div className="sub-text">
+          <span>What do your customers see...</span>
+          <span>...Are you SURE?</span>
+        </div>
       </div>
     </header>
   );
